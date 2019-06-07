05d0b8a983191f5b719aae29264aa076777abb67
valeria campilongo <cvaleriac@hotmail.it>
2019-06-07 18:42:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,23 +4,24 @@ class boardMember {
     this.homeState = homeState;
     this.training = training;
   }
+}
 
   veto = function() {
-    return 'No, I must disagree'
+    return 'No, I must disagree'}
 }
 
 approve = function() {
-    return 'You can do that!'
+    return 'You can do that!'}
 }
 
 doCharity = function() {
-    return "I like to help people."
+    return "I like to help people."}
 }
 
 releasePressStatement = function() {
-    return "You will see great things from Scuber."
+    return "You will see great things from Scuber."}
 }
 
 sayHi = function() {
-    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+    return "Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}."}
 }
